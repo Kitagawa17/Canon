a9f4bdab96a90a05c090ff4e7a29e2e8a7956f65
STUDENT\2212032 <yutatematsu@gmail.com>
2023-11-12 08:31:10 +0900

    Merge remote-tracking branch 'origin/main' into tate
    # Conflicts:
    #	Assets/Scenes/SampleScene.unity

--- Assets/Script/UI_MusicHand.cs
+++ Assets/Script/UI_MusicHand.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class UI_MusicHand : MonoBehaviour
 {
+    
+
     // Start is called before the first frame update
     [SerializeField] int musicNotes_Number;
     [SerializeField] Sound sound;
+    private void Start()
+    {
+        
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("SequenceBar"))
         {
-            Debug.Log("aaa");
             sound.SourceNumber(musicNotes_Number);
         }
     }
